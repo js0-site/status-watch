0615547bc91e08c357b7a9d3708c7e2583086fac
js0.site <jssite@googlegroups.com>
2025-11-18 16:06:44 +0800

    init

---
+++ setEnv.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+
+rm -f .dev.vars
+
+setEnv() {
+	echo "$1=\"$2\"" >>.dev.vars
+	echo "$2" | wrangler secret put $1
+}
+
+setEnv R "$(bun -e 'import R from "../conf/status/REDIS.js"; console.log(`redis://${R.username}:${R.password}@${R.host}:${R.port}/${R.db}`)')"
+
+setEnv SEND "$(bun -e 'import * as C from "../conf/status/SEND.js";console.log(JSON.stringify(C))')"
---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -x
+
+bun run dev &
+BUN_PID=$!
+
+while ! nc -z localhost 8787; do
+	sleep 1
+done
+
+curl "http://localhost:8787/__scheduled?cron=*+*+*+*+*"
+oxfmt
+oxlint
+sleep 3
+kill -INT $BUN_PID
+wait $BUN_PID
